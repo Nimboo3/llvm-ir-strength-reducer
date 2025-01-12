5f420e3c5fc86ffdcaf22bf91a8f234521dfd16a
Tanmay Patel <tptanmaypatel9@gmail.com>
2025-01-12 14:20:00 +0530

    Implement multiply-by-power-of-two optimization

--- src/StrengthReduce.cpp
+++ src/StrengthReduce.cpp
@@ -2,6 +2,7 @@
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/Support/raw_ostream.h"
+#include <cmath>
 
 using namespace llvm;
 
@@ -10,7 +11,31 @@ PreservedAnalyses StrengthReducePass::run(Function &F, FunctionAnalysisManager &
 
     for (auto &BB : F) {
         for (auto &I : BB) {
-            // TODO: Implement strength reduction transformations
+            // Match integer multiplication by constant
+            if (auto *Mul = dyn_cast<BinaryOperator>(&I)) {
+                if (Mul->getOpcode() == Instruction::Mul) {
+                    Value *Op0 = Mul->getOperand(0);
+                    Value *Op1 = Mul->getOperand(1);
+
+                    // Check if second operand is a constant power of two
+                    if (auto *Const = dyn_cast<ConstantInt>(Op1)) {
+                        uint64_t Val = Const->getZExtValue();
+
+                        // Power of two check: n & (n - 1) == 0
+                        if (Val > 0 && (Val & (Val - 1)) == 0) {
+                            unsigned ShiftAmt = log2(Val);
+
+                            IRBuilder<> B(Mul);
+                            Value *Shifted =
+                                B.CreateShl(Op0, ConstantInt::get(Const->getType(), ShiftAmt),
+                                            "mul_pow2");
+
+                            Mul->replaceAllUsesWith(Shifted);
+                            Modified = true;
+                        }
+                    }
+                }
+            }
         }
     }
 
