7e9c2613e4463d78b399faed2a68446953c1b4ab
Tanmay Patel <tptanmaypatel9@gmail.com>
2025-01-18 16:45:00 +0530

    Add unsigned division by power-of-two optimization

--- src/StrengthReduce.cpp
+++ src/StrengthReduce.cpp
@@ -11,11 +11,11 @@ PreservedAnalyses StrengthReducePass::run(Function &F, FunctionAnalysisManager &
 
     for (auto &BB : F) {
         for (auto &I : BB) {
-            // Match integer multiplication by constant
-            if (auto *Mul = dyn_cast<BinaryOperator>(&I)) {
-                if (Mul->getOpcode() == Instruction::Mul) {
-                    Value *Op0 = Mul->getOperand(0);
-                    Value *Op1 = Mul->getOperand(1);
+            if (auto *BO = dyn_cast<BinaryOperator>(&I)) {
+                // Match integer multiplication by constant
+                if (BO->getOpcode() == Instruction::Mul) {
+                    Value *Op0 = BO->getOperand(0);
+                    Value *Op1 = BO->getOperand(1);
 
                     // Check if second operand is a constant power of two
                     if (auto *Const = dyn_cast<ConstantInt>(Op1)) {
@@ -25,12 +25,31 @@ PreservedAnalyses StrengthReducePass::run(Function &F, FunctionAnalysisManager &
                         if (Val > 0 && (Val & (Val - 1)) == 0) {
                             unsigned ShiftAmt = log2(Val);
 
-                            IRBuilder<> B(Mul);
+                            IRBuilder<> B(BO);
                             Value *Shifted =
                                 B.CreateShl(Op0, ConstantInt::get(Const->getType(), ShiftAmt),
                                             "mul_pow2");
 
-                            Mul->replaceAllUsesWith(Shifted);
+                            BO->replaceAllUsesWith(Shifted);
+                            Modified = true;
+                        }
+                    }
+                }
+                // Match unsigned division by power of two
+                else if (BO->getOpcode() == Instruction::UDiv) {
+                    Value *Op0 = BO->getOperand(0);
+                    if (auto *Const = dyn_cast<ConstantInt>(BO->getOperand(1))) {
+                        uint64_t Val = Const->getZExtValue();
+
+                        if (Val > 0 && (Val & (Val - 1)) == 0) {
+                            unsigned ShiftAmt = log2(Val);
+
+                            IRBuilder<> B(BO);
+                            Value *Shifted =
+                                B.CreateLShr(Op0, ConstantInt::get(Const->getType(), ShiftAmt),
+                                             "udiv_pow2");
+
+                            BO->replaceAllUsesWith(Shifted);
                             Modified = true;
                         }
                     }
