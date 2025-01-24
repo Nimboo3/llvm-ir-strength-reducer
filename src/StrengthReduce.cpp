67f26f5759fb7fe5595ac9fc5369a3f9233e3aed
Tanmay Patel <tptanmaypatel9@gmail.com>
2025-01-24 11:15:00 +0530

    Add shift-add chain decomposition for low popcount constants

--- src/StrengthReduce.cpp
+++ src/StrengthReduce.cpp
@@ -1,10 +1,33 @@
 #include "StrengthReduce.h"
+#include "llvm/ADT/APInt.h"
+#include "llvm/ADT/SmallVector.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/Support/raw_ostream.h"
 #include <cmath>
 
 using namespace llvm;
+
+namespace {
+// Build a shift-add chain for x * C where C has few set bits
+Value *buildShiftAddChain(IRBuilder<> &B, Value *X, const APInt &C) {
+    SmallVector<Value *> Terms;
+    for (unsigned Bit = 0; Bit < C.getBitWidth(); ++Bit) {
+        if (C[Bit]) {
+            if (Bit == 0) {
+                Terms.push_back(X);
+            } else {
+                Terms.push_back(B.CreateShl(X, Bit));
+            }
+        }
+    }
+    Value *Acc = Terms[0];
+    for (unsigned i = 1; i < Terms.size(); ++i) {
+        Acc = B.CreateAdd(Acc, Terms[i]);
+    }
+    return Acc;
+}
+} // namespace
 
 PreservedAnalyses StrengthReducePass::run(Function &F, FunctionAnalysisManager &) {
     bool Modified = false;
@@ -17,20 +40,26 @@ PreservedAnalyses StrengthReducePass::run(Function &F, FunctionAnalysisManager &
                     Value *Op0 = BO->getOperand(0);
                     Value *Op1 = BO->getOperand(1);
 
-                    // Check if second operand is a constant power of two
                     if (auto *Const = dyn_cast<ConstantInt>(Op1)) {
-                        uint64_t Val = Const->getZExtValue();
+                        const APInt &C = Const->getValue();
+                        if (C.isNullValue() || C == 1) continue;
 
-                        // Power of two check: n & (n - 1) == 0
-                        if (Val > 0 && (Val & (Val - 1)) == 0) {
-                            unsigned ShiftAmt = log2(Val);
-
-                            IRBuilder<> B(BO);
+                        IRBuilder<> B(BO);
+                        
+                        // Power of two -> single shift
+                        if (C.isPowerOf2()) {
+                            unsigned ShiftAmt = C.logBase2();
                             Value *Shifted =
                                 B.CreateShl(Op0, ConstantInt::get(Const->getType(), ShiftAmt),
                                             "mul_pow2");
-
                             BO->replaceAllUsesWith(Shifted);
+                            Modified = true;
+                        }
+                        // Low popcount: use shift-add chain
+                        else if (C.countPopulation() <= 3 && C.ult(256)) {
+                            Value *Chain = buildShiftAddChain(B, Op0, C);
+                            Chain->setName("mul_addchain");
+                            BO->replaceAllUsesWith(Chain);
                             Modified = true;
                         }
                     }
@@ -39,16 +68,14 @@ PreservedAnalyses StrengthReducePass::run(Function &F, FunctionAnalysisManager &
                 else if (BO->getOpcode() == Instruction::UDiv) {
                     Value *Op0 = BO->getOperand(0);
                     if (auto *Const = dyn_cast<ConstantInt>(BO->getOperand(1))) {
-                        uint64_t Val = Const->getZExtValue();
+                        const APInt &C = Const->getValue();
 
-                        if (Val > 0 && (Val & (Val - 1)) == 0) {
-                            unsigned ShiftAmt = log2(Val);
-
+                        if (C.isPowerOf2()) {
+                            unsigned ShiftAmt = C.logBase2();
                             IRBuilder<> B(BO);
                             Value *Shifted =
                                 B.CreateLShr(Op0, ConstantInt::get(Const->getType(), ShiftAmt),
                                              "udiv_pow2");
-
                             BO->replaceAllUsesWith(Shifted);
                             Modified = true;
                         }
